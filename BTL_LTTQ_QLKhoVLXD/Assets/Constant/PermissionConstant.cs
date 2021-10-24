916fa57120636977c6e702bb3127c1aa5ea78b6d
annguyen <annguyen.it.utc@gmail.com>
2021-10-24 18:38:18 +0700

    Display components accords permission

---
+++ BTL_LTTQ_QLKhoVLXD/Assets/Constant/PermissionConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_LTTQ_QLKhoVLXD.Assets.Constant
+{
+    class PermissionConstant
+    {
+        public static readonly List<int> CreateAccount = new List<int>{ 1 };
+    }
+}
