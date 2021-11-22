afdeb47bf9ad8cc3252772ca79f2bdeea769dcf3
THAI MINH HIEP <thaiminhhiep2311@gmail.com>
2021-11-22 22:11:56 +0700

    commit form

--- BTL_LTTQ_QLKhoVLXD/Forms/AddEmployee/fAddEmployee.Designer.cs
+++ BTL_LTTQ_QLKhoVLXD/Forms/AddEmployee/fAddEmployee.Designer.cs
@@ -32,7 +32,7 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
         private void InitializeComponent()
         {
             this.lblDob = new System.Windows.Forms.Label();
-            this.grbInfo = new BmwGroupBox();
+            this.grbInfo = new BTL_LTTQ_QLKhoVLXD.Controls.GroupBox.BmwGroupBox();
             this.cboPosition = new System.Windows.Forms.ComboBox();
             this.lblPosition = new System.Windows.Forms.Label();
             this.txtPhone = new System.Windows.Forms.TextBox();
@@ -45,14 +45,14 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.txtName = new System.Windows.Forms.TextBox();
             this.lblName = new System.Windows.Forms.Label();
             this.lblAddress = new System.Windows.Forms.Label();
-            this.grbAccount = new BmwGroupBox();
+            this.grbAccount = new BTL_LTTQ_QLKhoVLXD.Controls.GroupBox.BmwGroupBox();
             this.chkShowPassword = new System.Windows.Forms.CheckBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.lblUsername = new System.Windows.Forms.Label();
             this.lblPassword = new System.Windows.Forms.Label();
-            this.btnCreate = new BmwButton();
-            this.btnCancel = new BmwButton();
+            this.btnCreate = new BTL_LTTQ_QLKhoVLXD.Controls.Button.BmwButton();
+            this.btnCancel = new BTL_LTTQ_QLKhoVLXD.Controls.Button.BmwButton();
             this.grbInfo.SuspendLayout();
             this.grbAccount.SuspendLayout();
             this.SuspendLayout();
@@ -62,12 +62,13 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.lblDob.AutoSize = true;
             this.lblDob.Location = new System.Drawing.Point(6, 100);
             this.lblDob.Name = "lblDob";
-            this.lblDob.Size = new System.Drawing.Size(71, 17);
+            this.lblDob.Size = new System.Drawing.Size(83, 20);
             this.lblDob.TabIndex = 2;
             this.lblDob.Text = "Ngày sinh";
             // 
             // grbInfo
             // 
+            this.grbInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(128)))), ((int)(((byte)(189)))));
             this.grbInfo.Controls.Add(this.cboPosition);
             this.grbInfo.Controls.Add(this.lblPosition);
             this.grbInfo.Controls.Add(this.txtPhone);
@@ -81,10 +82,13 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.grbInfo.Controls.Add(this.lblName);
             this.grbInfo.Controls.Add(this.lblDob);
             this.grbInfo.Controls.Add(this.lblAddress);
+            this.grbInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grbInfo.ForeColor = System.Drawing.Color.White;
             this.grbInfo.Location = new System.Drawing.Point(12, 12);
             this.grbInfo.Name = "grbInfo";
             this.grbInfo.Size = new System.Drawing.Size(465, 248);
             this.grbInfo.TabIndex = 3;
+            this.grbInfo.TabStop = false;
             this.grbInfo.Text = "Thông tin cá nhân";
             // 
             // cboPosition
@@ -93,7 +97,7 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.cboPosition.FormattingEnabled = true;
             this.cboPosition.Location = new System.Drawing.Point(138, 208);
             this.cboPosition.Name = "cboPosition";
-            this.cboPosition.Size = new System.Drawing.Size(312, 24);
+            this.cboPosition.Size = new System.Drawing.Size(312, 28);
             this.cboPosition.TabIndex = 12;
             // 
             // lblPosition
@@ -101,7 +105,7 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.lblPosition.AutoSize = true;
             this.lblPosition.Location = new System.Drawing.Point(6, 208);
             this.lblPosition.Name = "lblPosition";
-            this.lblPosition.Size = new System.Drawing.Size(59, 17);
+            this.lblPosition.Size = new System.Drawing.Size(70, 20);
             this.lblPosition.TabIndex = 11;
             this.lblPosition.Text = "Chức vụ";
             // 
@@ -119,7 +123,7 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.lblPhone.AutoSize = true;
             this.lblPhone.Location = new System.Drawing.Point(6, 175);
             this.lblPhone.Name = "lblPhone";
-            this.lblPhone.Size = new System.Drawing.Size(91, 17);
+            this.lblPhone.Size = new System.Drawing.Size(106, 20);
             this.lblPhone.TabIndex = 9;
             this.lblPhone.Text = "Số điện thoại";
             // 
@@ -128,7 +132,7 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.lblGender.AutoSize = true;
             this.lblGender.Location = new System.Drawing.Point(6, 138);
             this.lblGender.Name = "lblGender";
-            this.lblGender.Size = new System.Drawing.Size(60, 17);
+            this.lblGender.Size = new System.Drawing.Size(71, 20);
             this.lblGender.TabIndex = 8;
             this.lblGender.Text = "Giới tính";
             // 
@@ -137,7 +141,7 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.rdoFemale.AutoSize = true;
             this.rdoFemale.Location = new System.Drawing.Point(233, 138);
             this.rdoFemale.Name = "rdoFemale";
-            this.rdoFemale.Size = new System.Drawing.Size(47, 21);
+            this.rdoFemale.Size = new System.Drawing.Size(51, 24);
             this.rdoFemale.TabIndex = 7;
             this.rdoFemale.Text = "Nữ";
             this.rdoFemale.UseVisualStyleBackColor = true;
@@ -147,7 +151,7 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.rdoMale.AutoSize = true;
             this.rdoMale.Location = new System.Drawing.Point(138, 138);
             this.rdoMale.Name = "rdoMale";
-            this.rdoMale.Size = new System.Drawing.Size(58, 21);
+            this.rdoMale.Size = new System.Drawing.Size(65, 24);
             this.rdoMale.TabIndex = 6;
             this.rdoMale.Text = "Nam";
             this.rdoMale.UseVisualStyleBackColor = true;
@@ -189,7 +193,7 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.lblName.AutoSize = true;
             this.lblName.Location = new System.Drawing.Point(6, 31);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(69, 17);
+            this.lblName.Size = new System.Drawing.Size(81, 20);
             this.lblName.TabIndex = 0;
             this.lblName.Text = "Họ và tên";
             // 
@@ -198,21 +202,25 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.lblAddress.AutoSize = true;
             this.lblAddress.Location = new System.Drawing.Point(6, 66);
             this.lblAddress.Name = "lblAddress";
-            this.lblAddress.Size = new System.Drawing.Size(51, 17);
+            this.lblAddress.Size = new System.Drawing.Size(61, 20);
             this.lblAddress.TabIndex = 1;
             this.lblAddress.Text = "Địa chỉ";
             // 
             // grbAccount
             // 
+            this.grbAccount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(128)))), ((int)(((byte)(189)))));
             this.grbAccount.Controls.Add(this.chkShowPassword);
             this.grbAccount.Controls.Add(this.txtPassword);
             this.grbAccount.Controls.Add(this.txtUsername);
             this.grbAccount.Controls.Add(this.lblUsername);
             this.grbAccount.Controls.Add(this.lblPassword);
+            this.grbAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grbAccount.ForeColor = System.Drawing.Color.White;
             this.grbAccount.Location = new System.Drawing.Point(483, 12);
             this.grbAccount.Name = "grbAccount";
             this.grbAccount.Size = new System.Drawing.Size(465, 129);
             this.grbAccount.TabIndex = 13;
+            this.grbAccount.TabStop = false;
             this.grbAccount.Text = "Tài khoản";
             // 
             // chkShowPassword
@@ -220,7 +228,7 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.chkShowPassword.AutoSize = true;
             this.chkShowPassword.Location = new System.Drawing.Point(138, 96);
             this.chkShowPassword.Name = "chkShowPassword";
-            this.chkShowPassword.Size = new System.Drawing.Size(140, 21);
+            this.chkShowPassword.Size = new System.Drawing.Size(162, 24);
             this.chkShowPassword.TabIndex = 9;
             this.chkShowPassword.Text = "Hiển thị mật khẩu";
             this.chkShowPassword.UseVisualStyleBackColor = true;
@@ -230,9 +238,9 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             // 
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(128)))), ((int)(((byte)(189)))));
-            this.txtPassword.Location = new System.Drawing.Point(138, 55);
+            this.txtPassword.Location = new System.Drawing.Point(160, 55);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(312, 26);
+            this.txtPassword.Size = new System.Drawing.Size(290, 26);
             this.txtPassword.TabIndex = 8;
             this.txtPassword.UseSystemPasswordChar = true;
             // 
@@ -240,9 +248,9 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             // 
             this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(128)))), ((int)(((byte)(189)))));
-            this.txtUsername.Location = new System.Drawing.Point(138, 20);
+            this.txtUsername.Location = new System.Drawing.Point(160, 20);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(312, 26);
+            this.txtUsername.Size = new System.Drawing.Size(290, 26);
             this.txtUsername.TabIndex = 7;
             // 
             // lblUsername
@@ -250,7 +258,7 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.lblUsername.AutoSize = true;
             this.lblUsername.Location = new System.Drawing.Point(14, 26);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(105, 17);
+            this.lblUsername.Size = new System.Drawing.Size(119, 20);
             this.lblUsername.TabIndex = 5;
             this.lblUsername.Text = "Tên đăng nhập";
             // 
@@ -259,26 +267,36 @@ namespace BTL_LTTQ_QLKhoVLXD.Forms.AddEmployee
             this.lblPassword.AutoSize = true;
             this.lblPassword.Location = new System.Drawing.Point(14, 61);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(66, 17);
+            this.lblPassword.Size = new System.Drawing.Size(77, 20);
             this.lblPassword.TabIndex = 6;
             this.lblPassword.Text = "Mật khẩu";
             // 
             // btnCreate
             // 
+            this.btnCreate.BackColor = System.Drawing.Color.White;
+            this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCreate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(128)))), ((int)(((byte)(189)))));
             this.btnCreate.Location = new System.Drawing.Point(552, 187);
             this.btnCreate.Name = "btnCreate";
             this.btnCreate.Size = new System.Drawing.Size(129, 38);
             this.btnCreate.TabIndex = 14;
             this.btnCreate.Text = "Tạo tài khoản";
+            this.btnCreate.UseVisualStyleBackColor = false;
             this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // btnCancel
             // 
+            this.btnCancel.BackColor = System.Drawing.Color.White;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(128)))), ((int)(((byte)(189)))));
             this.btnCancel.Location = new System.Drawing.Point(738, 187);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(129, 38);
             this.btnCancel.TabIndex = 15;
             this.btnCancel.Text = "Hủy bỏ";
+            this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // fAddEmployee
