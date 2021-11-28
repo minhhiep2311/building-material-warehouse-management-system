8a3651b45f2d2d85e26a72ad11f411b5fcf5591b
THAI MINH HIEP <thaiminhhiep2311@gmail.com>
2021-11-28 14:04:40 +0700

    Fix Form fUnit

---
+++ BTL_LTTQ_QLKhoVLXD/Forms/Unit/fUnit.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace BTL_LTTQ_QLKhoVLXD.Forms.Unit
+{
+    partial class fUnit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUnit = new System.Windows.Forms.Label();
+            this.txtUnit = new System.Windows.Forms.TextBox();
+            this.pnlButton = new System.Windows.Forms.Panel();
+            this.btnCancel = new BTL_LTTQ_QLKhoVLXD.Controls.Button.BmwButton();
+            this.btnSave = new BTL_LTTQ_QLKhoVLXD.Controls.Button.BmwButton();
+            this.pnlUnit = new System.Windows.Forms.Panel();
+            this.pnlButton.SuspendLayout();
+            this.pnlUnit.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblUnit
+            // 
+            this.lblUnit.AutoSize = true;
+            this.lblUnit.Location = new System.Drawing.Point(19, 15);
+            this.lblUnit.Name = "lblUnit";
+            this.lblUnit.Size = new System.Drawing.Size(48, 17);
+            this.lblUnit.TabIndex = 0;
+            this.lblUnit.Text = "Đơn vị";
+            // 
+            // txtUnit
+            // 
+            this.txtUnit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUnit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(128)))), ((int)(((byte)(189)))));
+            this.txtUnit.Location = new System.Drawing.Point(96, 9);
+            this.txtUnit.Name = "txtUnit";
+            this.txtUnit.Size = new System.Drawing.Size(344, 26);
+            this.txtUnit.TabIndex = 4;
+            // 
+            // pnlButton
+            // 
+            this.pnlButton.Controls.Add(this.btnCancel);
+            this.pnlButton.Controls.Add(this.btnSave);
+            this.pnlButton.Location = new System.Drawing.Point(12, 118);
+            this.pnlButton.Name = "pnlButton";
+            this.pnlButton.Size = new System.Drawing.Size(464, 58);
+            this.pnlButton.TabIndex = 19;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.White;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(128)))), ((int)(((byte)(189)))));
+            this.btnCancel.Location = new System.Drawing.Point(291, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(110, 45);
+            this.btnCancel.TabIndex = 17;
+            this.btnCancel.Text = "Hủy bỏ";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.White;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(128)))), ((int)(((byte)(189)))));
+            this.btnSave.Location = new System.Drawing.Point(22, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(110, 45);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Lưu ";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // pnlUnit
+            // 
+            this.pnlUnit.Controls.Add(this.lblUnit);
+            this.pnlUnit.Controls.Add(this.txtUnit);
+            this.pnlUnit.Location = new System.Drawing.Point(12, 34);
+            this.pnlUnit.Name = "pnlUnit";
+            this.pnlUnit.Size = new System.Drawing.Size(464, 47);
+            this.pnlUnit.TabIndex = 20;
+            // 
+            // Unit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(128)))), ((int)(((byte)(189)))));
+            this.ClientSize = new System.Drawing.Size(497, 218);
+            this.Controls.Add(this.pnlUnit);
+            this.Controls.Add(this.pnlButton);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Unit";
+            this.Text = "Đơn vị";
+            this.pnlButton.ResumeLayout(false);
+            this.pnlUnit.ResumeLayout(false);
+            this.pnlUnit.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUnit;
+        private System.Windows.Forms.TextBox txtUnit;
+        private System.Windows.Forms.Panel pnlButton;
+        private Controls.Button.BmwButton btnCancel;
+        private Controls.Button.BmwButton btnSave;
+        private System.Windows.Forms.Panel pnlUnit;
+    }
+}
